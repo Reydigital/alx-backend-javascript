17b212927bef0f0676df0705c55071a44c11693b
Reydigital <regysmarie@gmail.com>
2022-12-19 23:32:02 +0000

    commit

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,13 @@
+export default function createIteratorObject(report) {
+
+	  const employees = [];
+
+	  for (const dpt of Object.values(report.allEmployees)) {
+
+		      employees.push(...dpt);
+
+		    }
+
+	  return employees;
+
+}
---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,17 @@
+export default function getBudgetObject(income, gdp, capita) {
+
+	  const budget = {
+
+		      income,
+
+		      gdp,
+
+		      capita,
+
+		    };
+
+
+
+	  return budget;
+
+}
